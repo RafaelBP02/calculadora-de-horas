cce14fc2262e04fc286f8947e72cf71c8072cb8c
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-07-24 18:06:20 -0300

    feat(new component): Add modal to show calculation result
    This opens a simple modal at the beginning of the code

---
+++ calculadora-horas-app/src/app/components/calc-result-dialog/calc-result-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calc-result-dialog',
+  templateUrl: './calc-result-dialog.component.html',
+  styleUrl: './calc-result-dialog.component.css'
+})
+export class CalcResultDialogComponent {
+
+}
--- calculadora-horas-app/src/app/components/calculadora/calculadora.component.ts
+++ calculadora-horas-app/src/app/components/calculadora/calculadora.component.ts
@@ -1,43 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Calculadora } from '../../models/Calculadora';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-calculadora',
   templateUrl: './calculadora.component.html',
-  styleUrl: './calculadora.component.css'
+  styleUrl: './calculadora.component.css',
 })
-export class CalculadoraComponent {
-  timeRegex:string = '^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$';
+export class CalculadoraComponent implements OnInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
-  calcFormData = new Calculadora()
+  timeRegex: string = '^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$';
 
-  calcularHoraio():void{
+  visible: boolean = false;
+  calcFormData = new Calculadora();
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      // O código aqui dentro só será executado no navegador
+      this.visible = true;
+      // this.initializeDialog();
+    }
+  }
+
+  displayDialog(open:boolean):void{
+    this.visible = open;
+  }
+
+  calcularHoraio(): void {
     // Calculo realizado para uma carga horária de oito horas diarias
-    let cargaHorariaRestante:Date = new Date();
-    let entradaT:Date = this.timeStringToDate(this.calcFormData.entrada);
-    let inicioT:Date = this.timeStringToDate(this.calcFormData.inicioIntervalo);
-    let fimT:Date = this.timeStringToDate(this.calcFormData.fimIntervalo);
+    let cargaHorariaRestante: Date = new Date();
+    let entradaT: Date = this.timeStringToDate(this.calcFormData.entrada);
+    let inicioT: Date = this.timeStringToDate(
+      this.calcFormData.inicioIntervalo
+    );
+    let fimT: Date = this.timeStringToDate(this.calcFormData.fimIntervalo);
 
     console.log(entradaT.getHours(), inicioT.getHours(), fimT.getHours());
 
+    cargaHorariaRestante.setMinutes(
+      0 - Math.abs(entradaT.getMinutes() - inicioT.getMinutes())
+    );
 
-    cargaHorariaRestante.setMinutes(0 - Math.abs(entradaT.getMinutes()-inicioT.getMinutes()));
-
-    cargaHorariaRestante.setHours(8 - Math.abs(entradaT.getHours()-inicioT.getHours()));
-
+    cargaHorariaRestante.setHours(
+      8 - Math.abs(entradaT.getHours() - inicioT.getHours())
+    );
 
     console.log('Carga horaria restante: ' + cargaHorariaRestante);
 
-    fimT.setHours(fimT.getHours()+cargaHorariaRestante.getHours());
-    fimT.setMinutes(fimT.getMinutes()+cargaHorariaRestante.getMinutes());
+    fimT.setHours(fimT.getHours() + cargaHorariaRestante.getHours());
+    fimT.setMinutes(fimT.getMinutes() + cargaHorariaRestante.getMinutes());
     // let resultadoT:Date = new Date();
     console.log('Horario de saida: ' + fimT);
-
 
     this.calcFormData = new Calculadora();
   }
 
-  cancelarOperacao():void{
+  cancelarOperacao(): void {
     this.calcFormData = new Calculadora();
   }
 
