4814ab5984b8c96e74020fe78504c11de3ce170c
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-07-24 11:09:44 -0300

    feat: calcula a diferença de horario
    Resultado printado no console

--- calculadora-horas-app/src/app/components/calculadora/calculadora.component.ts
+++ calculadora-horas-app/src/app/components/calculadora/calculadora.component.ts
@@ -7,11 +7,36 @@ import { Calculadora } from '../../models/Calculadora';
   styleUrl: './calculadora.component.css'
 })
 export class CalculadoraComponent {
+  timeRegex:string = '^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$';
 
   calcFormData = new Calculadora()
 
   calcularHoraio():void{
-    console.log(this.calcFormData);
+    // Calculo realizado para uma carga horária de oito horas diarias
+    let cargaHorariaRestante:number = 8;
+    let entradaT:Date = this.timeStringToDate(this.calcFormData.entrada);
+    let inicioT:Date = this.timeStringToDate(this.calcFormData.inicioIntervalo);
+    let fimT:Date = this.timeStringToDate(this.calcFormData.fimIntervalo);
+
+    console.log(entradaT.getHours(), inicioT.getHours(), fimT.getHours());
+
+    cargaHorariaRestante = (cargaHorariaRestante - Math.abs(entradaT.getHours()-inicioT.getHours()) - Math.abs(inicioT.getHours()-fimT.getHours()));
+    fimT.setHours(fimT.getHours()+cargaHorariaRestante);
+    // let resultadoT:Date = new Date();
+    console.log(cargaHorariaRestante, fimT.getHours());
+
+
     this.calcFormData = new Calculadora();
+  }
+
+  cancelarOperacao():void{
+    this.calcFormData = new Calculadora();
+  }
+
+  private timeStringToDate(time: string): Date {
+    const [hours, minutes] = time.split(':').map(Number);
+    const date = new Date();
+    date.setHours(hours, minutes, 0, 0);
+    return date;
   }
 }
