d72d6ba504d0092f85d89a887006836a8cf22eac
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-07-29 15:23:46 -0300

    feat: implement new workload radio buttons
    This allows the user to select his/hers workload beteen 8, 6 or 4 hours

--- calculadora-horas-app/src/app/app.module.ts
+++ calculadora-horas-app/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { CalcResultDialogComponent } from './components/calc-result-dialog/calc-result-dialog.component';
 import { DialogModule } from 'primeng/dialog';
+import { RadioButtonModule } from 'primeng/radiobutton';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { DialogModule } from 'primeng/dialog';
     InputTextModule,
     InputGroupModule,
     InputGroupAddonModule,
-    DialogModule
+    DialogModule,
+    RadioButtonModule
   ],
   providers: [
     provideClientHydration()
--- calculadora-horas-app/src/app/components/calculadora/calculadora.component.ts
+++ calculadora-horas-app/src/app/components/calculadora/calculadora.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Calculadora } from '../../models/Calculadora';
 
 @Component({
@@ -6,33 +6,61 @@ import { Calculadora } from '../../models/Calculadora';
   templateUrl: './calculadora.component.html',
   styleUrl: './calculadora.component.css',
 })
-export class CalculadoraComponent{
+export class CalculadoraComponent implements OnInit{
   constructor() {}
 
   visible: boolean = false;
-  horarioSaida: Date = new Date;
+  horarioSaida: Date = new Date();
   calcFormData = new Calculadora();
 
-  displayDialog():void{
+  cargaSelecionada: number = 8;
+
+  cargasHorarias: any[] = [
+    { name: '8 horas', value: 8, key: 'O' },
+    { name: '6 horas', value: 6, key: 'S' },
+    { name: '4 horas', value: 4, key: 'Q' },
+  ];
+
+  ngOnInit() {
+    // inicia a aplicação com 8 horas marcado por padrao
+    this.cargaSelecionada = this.cargasHorarias[0];
+  }
+
+  displayDialog(): void {
     this.visible = true;
   }
 
   calcularHoraio(): void {
     // Calculo realizado para uma carga horária de oito horas diarias
-    let cargaHorariaRestante:Date = new Date();
-    let horaEntrada:Date = this.converteStringParaDate(this.calcFormData.entrada);
-    let inicioIntervalo:Date = this.converteStringParaDate(this.calcFormData.inicioIntervalo);
-    let fimIntervalo:Date = this.converteStringParaDate(this.calcFormData.fimIntervalo);
+    let cargaHorariaRestante: Date = new Date();
+    let horaEntrada: Date = this.converteStringParaDate(
+      this.calcFormData.entrada
+    );
+    let inicioIntervalo: Date = this.converteStringParaDate(
+      this.calcFormData.inicioIntervalo
+    );
+    let fimIntervalo: Date = this.converteStringParaDate(
+      this.calcFormData.fimIntervalo
+    );
 
-    cargaHorariaRestante.setHours(8);
+    cargaHorariaRestante.setHours(this.cargaSelecionada);
     cargaHorariaRestante.setMinutes(0);
     cargaHorariaRestante.setSeconds(0);
 
-    cargaHorariaRestante.setMinutes( Math.abs(horaEntrada.getMinutes()-inicioIntervalo.getMinutes()));
-    cargaHorariaRestante.setHours(cargaHorariaRestante.getHours() - Math.abs(horaEntrada.getHours()-inicioIntervalo.getHours()));
+    cargaHorariaRestante.setMinutes(
+      Math.abs(horaEntrada.getMinutes() - inicioIntervalo.getMinutes())
+    );
+    cargaHorariaRestante.setHours(
+      cargaHorariaRestante.getHours() -
+        Math.abs(horaEntrada.getHours() - inicioIntervalo.getHours())
+    );
 
-    fimIntervalo.setHours(fimIntervalo.getHours()+cargaHorariaRestante.getHours());
-    fimIntervalo.setMinutes(fimIntervalo.getMinutes()+cargaHorariaRestante.getMinutes());
+    fimIntervalo.setHours(
+      fimIntervalo.getHours() + cargaHorariaRestante.getHours()
+    );
+    fimIntervalo.setMinutes(
+      fimIntervalo.getMinutes() + cargaHorariaRestante.getMinutes()
+    );
 
     this.horarioSaida = fimIntervalo;
 
