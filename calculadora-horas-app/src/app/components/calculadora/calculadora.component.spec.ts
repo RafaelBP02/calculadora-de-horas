b6bf279fda3411ad4967408d888a3c9a7fc07838
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-08-01 11:02:53 -0300

    test: test clicking on button 'cancelar'

--- calculadora-horas-app/src/app/components/calculadora/calculadora.component.spec.ts
+++ calculadora-horas-app/src/app/components/calculadora/calculadora.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { CalculadoraComponent } from './calculadora.component';
 import { DropdownModule } from 'primeng/dropdown';
@@ -10,6 +10,7 @@ import { ButtonModule } from 'primeng/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CalcResultDialogComponent } from '../calc-result-dialog/calc-result-dialog.component';
+import { By } from '@angular/platform-browser';
 
 describe('CalculadoraComponent', () => {
   let component: CalculadoraComponent;
@@ -18,7 +19,7 @@ describe('CalculadoraComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CalculadoraComponent, CalcResultDialogComponent],
-      imports:[
+      imports: [
         ButtonModule,
         BrowserAnimationsModule,
         FormsModule,
@@ -28,9 +29,8 @@ describe('CalculadoraComponent', () => {
         InputGroupAddonModule,
         DialogModule,
         DropdownModule,
-      ]
-    })
-    .compileComponents();
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CalculadoraComponent);
     component = fixture.componentInstance;
@@ -39,5 +39,42 @@ describe('CalculadoraComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve clicar no botao de cancelar', () => {
+
+    spyOn(component, 'cancelarOperacao');
+
+    let btnApagar: HTMLButtonElement = fixture.debugElement.query(By.css('#btnApagar > button')).nativeElement;
+    btnApagar.click();
+
+    expect(component.cancelarOperacao).toHaveBeenCalled();
+  });
+
+  xit('deve abrir o modal', async () => {
+    const horario: Date = new Date();
+    const btnConfirmar = fixture.debugElement.query(By.css('#btnCalcular'));
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    horario.setHours(18);
+    horario.setMinutes(0);
+    horario.setSeconds(0);
+
+    component.horaExcedida = false;
+    component.horarioSaida = horario;
+
+    spyOn(component, 'displayDialog').and.callThrough();
+
+    btnConfirmar.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    await fixture.whenStable();
+
+    expect(component.displayDialog).toHaveBeenCalled();
+
+    expect(component.visible).toBeTrue();
+
+    const dialogText = compiled.querySelector('p-dialog span')?.textContent;
+    expect(dialogText).toContain('Hora trabalhada excedida!');
   });
 });
