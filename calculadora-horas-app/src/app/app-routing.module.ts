87dd6a7eafd993e9426a13320658a5929a497d69
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-08-05 15:34:04 -0300

    feat: create notification config module

--- calculadora-horas-app/src/app/app-routing.module.ts
+++ calculadora-horas-app/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'notification-config', loadChildren: () => import('./modules/notify-config/notify-config.module').then(m => m.NotifyConfigModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
