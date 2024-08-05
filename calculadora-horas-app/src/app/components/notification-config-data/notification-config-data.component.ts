9dff20d5a895a692fe6bc29a1d1ae26607f4ebdd
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-08-05 17:12:30 -0300

    feat: add arrow button to return to the calculator

--- calculadora-horas-app/src/app/components/notification-config-data/notification-config-data.component.ts
+++ calculadora-horas-app/src/app/components/notification-config-data/notification-config-data.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-notification-config-data',
   templateUrl: './notification-config-data.component.html',
   styleUrl: './notification-config-data.component.css'
 })
-export class NotificationConfigDataComponent {
+export class NotificationConfigDataComponent implements OnInit {
+  formGroup: FormGroup | undefined;
 
+  ngOnInit() {
+      this.formGroup = new FormGroup({
+          inicioExpediente: new FormControl<string | null>(null)
+      });
+  }
 }
--- calculadora-horas-app/src/app/modules/notify-config/notify-config.module.ts
+++ calculadora-horas-app/src/app/modules/notify-config/notify-config.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NotifyConfigRoutingModule } from './notify-config-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    NotifyConfigRoutingModule
+    NotifyConfigRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class NotifyConfigModule { }
