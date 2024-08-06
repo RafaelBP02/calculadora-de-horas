ded54f78a11fc69166b2ccd0465b464f1f045f47
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-08-06 15:17:06 -0300

    test: test notification-config-data

--- calculadora-horas-app/src/app/components/notification-config-data/notification-config-data.component.spec.ts
+++ calculadora-horas-app/src/app/components/notification-config-data/notification-config-data.component.spec.ts
@@ -11,6 +11,8 @@ import { RouterModule } from '@angular/router';
 
 describe('NotificationConfigDataComponent', () => {
   let component: NotificationConfigDataComponent;
+  let confirmationService: ConfirmationService;
+  let messageService: MessageService;
   let fixture: ComponentFixture<NotificationConfigDataComponent>;
 
   beforeEach(async () => {
@@ -25,15 +27,47 @@ describe('NotificationConfigDataComponent', () => {
         RouterModule.forRoot([]),
       ],
       providers: [ConfirmationService, MessageService],
-    })
-    .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(NotificationConfigDataComponent);
     component = fixture.componentInstance;
+    confirmationService = TestBed.inject(ConfirmationService);
+    messageService = TestBed.inject(MessageService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('Formulario validado', () => {
+    beforeEach(() => {
+      component.horariosForm.controls.inicioExpediente.setValue('09:00');
+      component.horariosForm.controls.inicioIntervalo.setValue('13:00');
+      component.horariosForm.controls.fimIntervalo.setValue('14:00');
+      component.horariosForm.controls.fimExpediente.setValue('18:00');
+    });
+
+    it('deve confirmar os dados', () => {
+      expect(component.confirmaDadosSalvos()).toBeTruthy();
+    });
+    it('deve abrir dialogo', () => {
+      spyOn(confirmationService, 'confirm');
+      component.confirmarDados(new Event('click'));
+      expect(confirmationService.confirm).toHaveBeenCalled();
+    });
+    xit('deve aceitar os dados no dialogo', () => {});
+    xit('deve rejeitar os dados no dialogo', () => {});
+  });
+
+  describe('Formulario nao validado', () => {
+    it('deve cancelar os dados', () => {
+      expect(component.confirmaDadosSalvos()).toBeFalsy();
+    });
+    it('nao deve abrir o dialogo', () => {
+      component.confirmarDados(new Event('click'));
+      expect(component.confirmaDadosSalvos()).toBeFalsy();
+    });
+  });
+
 });
