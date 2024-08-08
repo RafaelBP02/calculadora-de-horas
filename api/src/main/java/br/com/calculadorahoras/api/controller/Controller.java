367ee0166608f0bdf01296a4de8cb160529a0bcb
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-08-08 17:57:35 -0300

    chore: configure database communication

--- api/src/main/java/br/com/calculadorahoras/api/controller/Controller.java
+++ api/src/main/java/br/com/calculadorahoras/api/controller/Controller.java
@@ -1,5 +1,14 @@
 package br.com.calculadorahoras.api.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
 public class Controller {
 
+    @GetMapping("/")
+    public String testConfig(){
+        return "Hello World!";
+    }
 }
