d902c165e65b3b9b2ea3b9fa35d8c98b57e1b9b9
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-08-09 15:52:23 -0300

    feat: creat form group conversion mothod

--- calculadora-horas-app/src/app/models/ConfigAlerta.ts
+++ calculadora-horas-app/src/app/models/ConfigAlerta.ts
@@ -1,3 +1,4 @@
+import { FormGroup } from '@angular/forms';
 export class ConfigAlerta{
     id:number = 0;
     workEntry:string = '';
@@ -6,4 +7,18 @@ export class ConfigAlerta{
     workEnd:string = '';
     workload:number = 0;
     user_id:number = 0;
+
+    static converteFormulario(formulario:FormGroup, id:number, user_id:number):ConfigAlerta{
+      const dadosConvertidos  = new ConfigAlerta();
+
+      dadosConvertidos.id = id;
+      dadosConvertidos.workEntry = formulario.get('inicioExpediente')?.value || '';
+      dadosConvertidos.intervalBeginning = formulario.get('inicioIntervalo')?.value || '';
+      dadosConvertidos.intervalEnd = formulario.get('fimIntervalo')?.value || '';
+      dadosConvertidos.workEnd = formulario.get('fimExpediente')?.value || '';
+      dadosConvertidos.workload = formulario.get('cargaHorariaSelecionada')?.value?.valor || 0;
+      dadosConvertidos.user_id = user_id;
+
+      return dadosConvertidos;
+    }
 }
