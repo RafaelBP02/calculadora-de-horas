b038d39666d49d9ba6fb59acd779505300d343f5
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-08-09 10:50:02 -0300

    feat: create REST routes for frontback integration

--- api/src/main/java/br/com/calculadorahoras/api/controller/Controller.java
+++ api/src/main/java/br/com/calculadorahoras/api/controller/Controller.java
@@ -1,14 +1,41 @@
 package br.com.calculadorahoras.api.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.calculadorahoras.api.model.AlertConfig;
+import br.com.calculadorahoras.api.repo.Repo;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
 public class Controller {
 
+    @Autowired
+    private Repo action;
+
     @GetMapping("/")
-    public String testConfig(){
-        return "Hello World!";
+    public Iterable<AlertConfig> selectAllAlarmConfigs(){
+        return action.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public void selectAlarmConfig(@PathVariable int id){
+        action.findById(id);
+    }
+
+    @PostMapping("/")
+    public AlertConfig registerAlarmConfig(@RequestBody AlertConfig ac){
+        return action.save(ac);
+    }
+
+    @PutMapping("/")
+    public AlertConfig editAlarmConfig(@RequestBody AlertConfig ac){
+        return action.save(ac);
     }
 }
