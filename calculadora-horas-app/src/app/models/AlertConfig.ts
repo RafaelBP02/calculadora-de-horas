86fa178999def76c8003898d1bef2284d7e2986d
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-08-09 11:09:09 -0300

    feat: create AlertConfig model

---
+++ calculadora-horas-app/src/app/models/AlertConfig.ts
@@ -0,0 +1,9 @@
+export class AlertConfig{
+    id:number = 0;
+    workEntry:string = '';
+    intervalBeginning:string = '';
+    intervalEnd:string = '';
+    workEnd:string = '';
+    workload:number = 0;
+    user_id:number = 0;
+}
