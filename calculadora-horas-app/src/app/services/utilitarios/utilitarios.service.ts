80d15025b94a0edcef2fca04cd46b77c0fc311fd
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-08-20 10:37:31 -0300

    refact: move format date to UtilitariosService

--- calculadora-horas-app/src/app/services/utilitarios/utilitarios.service.ts
+++ calculadora-horas-app/src/app/services/utilitarios/utilitarios.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { formatDate } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,12 @@ export class UtilitariosService {
     const date = new Date();
     date.setHours(horas, minutos, 0, 0);
     return date;
+  }
+
+  static formatTime(value: any): string {
+    const locale:string = 'pt-BR';
+
+    if (!value) return '';
+    return formatDate(UtilitariosService.converteStringParaDate(value), 'HH:mm:ss', locale);
   }
 }
