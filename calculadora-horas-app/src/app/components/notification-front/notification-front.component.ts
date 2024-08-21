cdce80fca0226ba8e70ae2e9d2ecae66828d1d88
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-08-21 11:29:33 -0300

    fix: add button to fix angular timeout

--- calculadora-horas-app/src/app/components/notification-front/notification-front.component.ts
+++ calculadora-horas-app/src/app/components/notification-front/notification-front.component.ts
@@ -1,8 +1,9 @@
 import { MessageService } from 'primeng/api';
-import { Component, NgZone, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, NgZone, OnDestroy, OnInit } from '@angular/core';
 import { ConfigAlertaService } from '../../services/config-alerta.service';
 import { ConfigAlerta } from '../../models/ConfigAlerta';
 import { UtilitariosService } from '../../services/utilitarios/utilitarios.service';
+import { Subscription, timer } from 'rxjs';
 
 @Component({
   selector: 'app-notification-front',
@@ -10,58 +11,62 @@ import { UtilitariosService } from '../../services/utilitarios/utilitarios.servi
   styleUrl: './notification-front.component.css',
 })
 export class NotificationFrontComponent implements OnInit, OnDestroy {
+  private intervalID: Subscription = new Subscription();
   private userId: number;
-  intervalID: any;
 
   configuracoesSalvas: ConfigAlerta = new ConfigAlerta();
+  botaoVisivel: boolean = false
 
-  constructor(private alertAPI: ConfigAlertaService, private messageService:MessageService, private ngZone:NgZone) {
+  constructor(private alertAPI: ConfigAlertaService, private messageService:MessageService) {
     this.userId = 2;
   }
 
   ngOnInit(): void {
-    this.ngZone.runOutsideAngular(() => {
-      this.alertAPI.selecionarAlerta(this.userId).subscribe({
-        next: (alertas) => {
-          this.ngZone.run(() => {
-            this.configuracoesSalvas = alertas;
-            // this.verificarAlertas();//esta dando timeout
-          });
-        },
-        error: (erro) => {
-          console.log('ERRO: ' + erro.message);
-        },
-      });
+    this.alertAPI.selecionarAlerta(this.userId).subscribe({
+      next: (alertas) => {
+        this.configuracoesSalvas = alertas;
+        this.botaoVisivel = true
+        console.log(this.configuracoesSalvas)
+
+      },
+      error: (erro) => {
+        console.log('ERRO: ' + erro.message);
+      },
     });
   }
 
   ngOnDestroy(): void {
-    if(this.intervalID){
-      clearInterval(this.intervalID);
+    if (this.intervalID) {
+      this.intervalID.unsubscribe();
     }
+  }
+
+  tarefaRelogio(): void {
+    this.intervalID = timer(0, 60000).subscribe(() => {
+      this.verificarAlertas();
+      console.log('-----------------------------');
+    });
   }
 
   private verificarAlertas(): void {
     const agora:Date = new Date()
 
-    this.intervalID = setInterval(() => {
-      if(this.calculaDiferencaMinutos(
-        UtilitariosService.converteStringParaDate(this.configuracoesSalvas.workEntry), agora, 2)){
-          this.showToastMessage("ATENÇÃO! ESTÁ NA HORA DE BATER O PONTO DE ENTRADA NO TRABALHO!")
+    if(this.calculaDiferencaMinutos(
+      UtilitariosService.converteStringParaDate(this.configuracoesSalvas.workEntry), agora, 2)){
+        this.showToastMessage("ATENÇÃO! ESTÁ NA HORA DE BATER O PONTO DE ENTRADA NO TRABALHO!")
+    }
+    else if(this.calculaDiferencaMinutos(
+      UtilitariosService.converteStringParaDate(this.configuracoesSalvas.intervalBeginning), agora, 2)){
+        this.showToastMessage("ATENÇÃO! ESTÁ NA HORA DE BATER O PONTO DE INICIO DO INTERVALO!")
+    }
+    else if(this.calculaDiferencaMinutos(
+      UtilitariosService.converteStringParaDate(this.configuracoesSalvas.intervalEnd), agora, 2)){
+        this.showToastMessage("ATENÇÃO! ESTÁ NA HORA DE BATER O PONTO DE FIM DO INTERVALO!")
+    }
+    else if(this.calculaDiferencaMinutos(
+      UtilitariosService.converteStringParaDate(this.configuracoesSalvas.workEnd), agora, 2)){
+        this.showToastMessage("ATENÇÃO! ESTÁ NA HORA DE BATER O PONTO DE SAÍDA NO TRABALHO!")
       }
-      else if(this.calculaDiferencaMinutos(
-        UtilitariosService.converteStringParaDate(this.configuracoesSalvas.intervalBeginning), agora, 2)){
-          this.showToastMessage("ATENÇÃO! ESTÁ NA HORA DE BATER O PONTO DE INICIO DO INTERVALO!")
-      }
-      if(this.calculaDiferencaMinutos(
-        UtilitariosService.converteStringParaDate(this.configuracoesSalvas.intervalEnd), agora, 2)){
-          this.showToastMessage("ATENÇÃO! ESTÁ NA HORA DE BATER O PONTO DE FIM DO INTERVALO!")
-      }
-      if(this.calculaDiferencaMinutos(
-        UtilitariosService.converteStringParaDate(this.configuracoesSalvas.workEnd), agora, 2)){
-          this.showToastMessage("ATENÇÃO! ESTÁ NA HORA DE BATER O PONTO DE SAÍDA NO TRABALHO!")
-        }
-    }, 60000); // verifica a cada minuto
   }
 
   private showToastMessage(mensagem: string): void {
@@ -73,7 +78,9 @@ export class NotificationFrontComponent implements OnInit, OnDestroy {
   }
 
   private calculaDiferencaMinutos(horario1: Date, horario2: Date, minutos: number): boolean{
-    //TODO logica para determinar se o intervalo de minutos é verdadeiro
-    return true;
+    const diffMs = Math.abs(horario1.getTime() - horario2.getTime());
+    const diffMins = Math.ceil(diffMs / (1000 * 60));
+    console.log('diferenca = ' + diffMins + ' minutos');
+    return diffMins <= minutos;
   }
 }
