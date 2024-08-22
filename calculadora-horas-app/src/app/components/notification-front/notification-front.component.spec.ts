9490c5fdddd769335fece14ee6fdb75640a3d3ce
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-08-22 11:56:31 -0300

    test: test get request

--- calculadora-horas-app/src/app/components/notification-front/notification-front.component.spec.ts
+++ calculadora-horas-app/src/app/components/notification-front/notification-front.component.spec.ts
@@ -55,57 +55,6 @@ describe('NotificationFrontComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  xdescribe('testa alertas dos horarios', () => {
-    let espiao: jasmine.Spy;
-
-    beforeEach(() => {
-      espiao = spyOn(timer(0, 60000), 'subscribe').and.callThrough();
-      component.LigaDesliga = true;
-      component.tarefaRelogio();
-    });
-
-    afterEach(() => {
-      espiao.calls.reset();
-    });
-
-    it('deve testar horario de entrada', fakeAsync(() => {
-      const request = httpTestingController.expectOne(
-        (data) =>
-          data.url === 'http://localhost:8080/2' && data.method === 'GET'
-      );
-
-      request.flush(mockOneAlert);
-
-      spyOn<any>(component, 'showToastMessage').and.callThrough();
-      spyOn(messageService, 'add');
-
-      fixture.detectChanges();
-
-      let btnApagar: HTMLButtonElement = fixture.debugElement.query(
-        By.css('#liga-deliga')
-      ).nativeElement;
-      btnApagar.click();
-
-      fixture.detectChanges();
-
-      component.LigaDesliga = true;
-
-      component.tarefaRelogio();
-
-      expect(UtilitariosService.converteStringParaDate(component.configuracoesSalvas.workEntry)).toEqual(new Date);
-
-      expect(messageService.add).toHaveBeenCalledWith({
-        severity: 'info',
-        summary: 'Nota:',
-        detail:
-          'ATENÇÃO! ESTÁ NA HORA DE BATER O PONTO DE ENTRADA NO TRABALHO!',
-      });
-
-      tick(1000);
-      component.intervalID.unsubscribe();
-    }));
-  });
-
   xit('deve testar horario de entrada do intervalo', () => {
     let originalDate: DateConstructor;
 
@@ -157,6 +106,18 @@ describe('NotificationFrontComponent', () => {
   // it('deve testar horario de saida do intervalo', () => {});
   // it('deve testar horario de saida', () => {});
 
+  it('deve receber o alerta configurado', () => {
+    const request = httpTestingController.expectOne(
+      (data) => data.url === 'http://localhost:8080/2' && data.method === 'GET'
+    );
+
+    request.flush(mockOneAlert);
+
+    component.ngOnInit();
+    expect(component.configuracoesSalvas).toBeDefined();
+    expect(component.botaoVisivel).toBeTrue();
+  });
+
   it('deve ligar e deligar os alertas', fakeAsync(() => {
     spyOn(timer(0, 60000), 'subscribe').and.callThrough();
     spyOn<any>(component, 'calculaDiferencaMinutos').and.returnValue(false);
@@ -165,9 +126,11 @@ describe('NotificationFrontComponent', () => {
     component.tarefaRelogio();
     expect(component.intervalID).toBeTruthy;
 
-    tick(100);
+    tick(1000);
     component.LigaDesliga = false;
     component.tarefaRelogio();
     expect(component.intervalID.closed).toBeTruthy;
+    tick(1000);
+    component.intervalID.unsubscribe();
   }));
 });
