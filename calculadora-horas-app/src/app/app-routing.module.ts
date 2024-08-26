e252fbdb41bbe37f38369c10ec631658d15d7bde
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-08-26 15:26:30 -0300

    feat: create home component and update url paths

--- calculadora-horas-app/src/app/app-routing.module.ts
+++ calculadora-horas-app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CalculadoraComponent } from './components/calculadora/calculadora.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: CalculadoraComponent},
+  {path: '', component: HomeComponent},
+  { path: 'calculadora', component: CalculadoraComponent},
   {path: 'notify-config', loadChildren: () => import('./modules/notify-config/notify-config.module').then(m => m.NotifyConfigModule)}
 ];
 
