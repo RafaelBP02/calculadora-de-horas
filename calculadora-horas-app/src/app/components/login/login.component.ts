25fbabad3eedc5835a8210a8f9bdcacaaa5cc5e2
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-09-04 11:49:10 -0300

    feat: make user login

--- calculadora-horas-app/src/app/components/login/login.component.ts
+++ calculadora-horas-app/src/app/components/login/login.component.ts
@@ -1,3 +1,5 @@
+import { UtilitariosService } from './../../services/utilitarios/utilitarios.service';
+import { AutorizacaoService, loginUsuario } from './../../services/autorizacao/autorizacao.service';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
@@ -8,17 +10,22 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent {
 
+  constructor(private autorizacaoService: AutorizacaoService){}
+
   loginForm = new FormGroup({
-    usuario: new FormControl<String>('', Validators.required),
-    senha: new FormControl<String>('', Validators.required)
+    usuario: new FormControl<string>('', Validators.required),
+    senha: new FormControl<string>('', Validators.required)
   })
 
   logarUsuario(event: Event):void{
     event.preventDefault();
 
     if (this.loginForm.valid) {
-      console.log('tenta fazer loggin...')
-    //TODO realizar logica para loggin
+      try {
+        this.configuraHeadersAutorizacao();
+      } catch (error) {
+        console.log('Este usuario nao existe')
+      }
     } else {
       this.loginForm.markAllAsTouched();
       console.log('EERO! CAMPOS DEVEM SER PREENCHIDOS');
@@ -29,5 +36,25 @@ export class LoginComponent {
     return this.loginForm.controls;
   }
 
+  configuraHeadersAutorizacao():void{
+    const usuario: loginUsuario = {
+      username: this.loginForm.controls.usuario.value || '',
+      password: this.loginForm.controls.senha.value || '',
+    };
+    this.autorizacaoService.efetuarLogin(usuario).subscribe({
+      next: (bearearToken) => {
+        UtilitariosService.setToken(bearearToken)
+        console.log('bem vindo')
+      },
+      error: (e: any) => {
+        console.error('Erro ao efetuar login:', e);
+
+      },
+    });
+  }
+
+
 
 }
+
+
