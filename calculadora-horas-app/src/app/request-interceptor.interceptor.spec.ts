192cff4502dfe7dad6a3024873b0ab0e3001992d
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-09-06 12:02:32 -0300

    test: auth guard route protection

--- calculadora-horas-app/src/app/request-interceptor.interceptor.spec.ts
+++ calculadora-horas-app/src/app/request-interceptor.interceptor.spec.ts
@@ -1,27 +1,41 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpRequest, provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import { RequestInterceptor } from './request-interceptor.interceptor';
+import { BrowserStorageService } from './services/browser-storage/browser-storage.service';
 
 describe('RequestInterceptor', () => {
+  let httpMock: HttpTestingController;
   let interceptor: RequestInterceptor;
+  let browserStorageService: BrowserStorageService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [
+        RequestInterceptor,
         provideHttpClient(withFetch(), withInterceptorsFromDi()),
-        {
-          provide: HTTP_INTERCEPTORS,
-          useClass: RequestInterceptor,
-          multi: true
-        }
+        { provide: HTTP_INTERCEPTORS, useClass: RequestInterceptor, multi: true },
+        BrowserStorageService
       ]
     });
 
-     const interceptors = TestBed.inject(HTTP_INTERCEPTORS);
-     interceptor = interceptors.find((i) => i instanceof RequestInterceptor) as RequestInterceptor;
+    httpMock = TestBed.inject(HttpTestingController);
+    interceptor = TestBed.inject(RequestInterceptor);
+    browserStorageService = TestBed.inject(BrowserStorageService);
   });
 
-  it('should be created', () => {
-    expect(interceptor).toBeTruthy();
+  it('deve adicionar o header de autorizacao', () => {
+    spyOn(browserStorageService, 'get').and.returnValue('token');
+
+    const req = new HttpRequest('GET', 'http://localhost/api');
+    const handleSpy = jasmine.createSpy('handle');
+
+    interceptor.intercept(req, <any>{ handle: handleSpy });
+
+    expect(handleSpy).toHaveBeenCalledWith(req.clone({
+      headers: req.headers.set('Authorization', 'Bearer token')
+    }));
   });
 });
+
--- calculadora-horas-app/src/app/services/auth-guard/auth-guard.service.spec.ts
+++ calculadora-horas-app/src/app/services/auth-guard/auth-guard.service.spec.ts
@@ -1,10 +1,13 @@
+import { AutorizacaoService } from './../autorizacao/autorizacao.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { AuthGuard } from './auth-guard.service';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 describe('AuthGuardService', () => {
   let service: AuthGuard;
+  let autorizacaoService:AutorizacaoService
+  let router: Router;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -17,9 +20,33 @@ describe('AuthGuardService', () => {
       ]
     });
     service = TestBed.inject(AuthGuard);
+    router = TestBed.inject(Router);
+    autorizacaoService = TestBed.inject(AutorizacaoService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('deve liberar a rota', () => {
+    spyOn(service, 'canActivate').and.callThrough();
+    spyOn(autorizacaoService, 'autenticado').and.returnValue(true);
+
+    let mudarRota: boolean;
+
+    mudarRota = service.canActivate();
+
+    expect(mudarRota).toBeTruthy();
+  });
+
+  it('nao deve liberar a rota', () => {
+    spyOn(service, 'canActivate').and.callThrough();
+    spyOn(autorizacaoService, 'autenticado').and.returnValue(false);
+
+    let mudarRota: boolean;
+
+    mudarRota = service.canActivate();
+
+    expect(mudarRota).toBeFalsy();
   });
 });
