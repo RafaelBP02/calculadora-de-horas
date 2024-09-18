b2ea59bd7d248c67f7a22f9756a77e8ad8ba2313
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-09-18 17:20:04 -0300

    feat: add REST requisition to signup

--- calculadora-horas-app/src/app/components/cadastro/cadastro.component.ts
+++ calculadora-horas-app/src/app/components/cadastro/cadastro.component.ts
@@ -1,6 +1,8 @@
+import { AutorizacaoService } from './../../services/autorizacao/autorizacao.service';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Usuario } from '../../models/Usuario';
 
 @Component({
   selector: 'app-cadastro',
@@ -8,7 +10,7 @@ import { Router } from '@angular/router';
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
-  constructor( private router: Router){}
+  constructor(private autorizacaoService:AutorizacaoService, private router: Router){}
 
   cadastroForm = new FormGroup({
     email: new FormControl<string>('', Validators.required),
@@ -25,6 +27,25 @@ export class CadastroComponent {
 
   cadastrarUsuario(event: Event):void{
     event.preventDefault();
-    //TODO POST REQUEST
+
+    let novoUsuario: Usuario = new Usuario;
+
+    novoUsuario.id = 0;
+    novoUsuario.username = this.cadastroForm.controls.email.value ?? '';
+    novoUsuario.name = this.cadastroForm.controls.nome.value ?? '';
+    novoUsuario.sureName = this.cadastroForm.controls.sobrenome.value ?? '';
+    novoUsuario.password = this.cadastroForm.controls.senha.value ?? '';
+    novoUsuario.workPlace = this.cadastroForm.controls.localTrabalho.value ?? '';
+    novoUsuario.roleId = 1;
+
+    this.autorizacaoService.efetuarSignup(novoUsuario).subscribe({
+      next: () => {
+        console.log('sucesso');
+        this.router.navigate(['/autenticacao/login']);
+      },
+      error: (e) => {
+        console.error('Erro ao efetuar login:', e);
+      }
+    })
   }
 }
