c86931b8531eeeb7b831bb877b87ad389a9ff281
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-09-18 15:57:55 -0300

    feat: create signup form

--- calculadora-horas-app/src/app/components/cadastro/cadastro.component.ts
+++ calculadora-horas-app/src/app/components/cadastro/cadastro.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
+  constructor( private router: Router){}
 
+  cadastroForm = new FormGroup({
+    email: new FormControl<string>('', Validators.required),
+    nome: new FormControl<string>('', Validators.required),
+    sobrenome: new FormControl<string>('', Validators.required),
+    localTrabalho: new FormControl<string>('', Validators.required),
+    senha: new FormControl<string>('', Validators.required),
+    confimarSenha: new FormControl<string>('', Validators.required)
+  })
+
+  get cadastroFormControl() {
+    return this.cadastroForm.controls;
+  }
+
+  cadastrarUsuario(event: Event):void{
+    event.preventDefault();
+    //TODO POST REQUEST
+  }
 }
